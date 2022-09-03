712776e8478d327cd4b1d06b93b06a0ac3cd89b9
rafaelhelebroke <112710912+rafaelhelebroke@users.noreply.github.com>
2022-09-03 16:55:34 -0300

    Add files via upload

---
+++ param/config.php
@@ -0,0 +1,84 @@
+<?php
+//ARQUIVO DE CONFIGURAÇÕES 
+
+//ATENÇÃO! MODIFIQUE SOMENTE O QUE FOR ORIENTADO, 
+//CASO MODIFIQUE ALGUM OUTRO LOCAL SEUS PARÂMETROS PODEM 
+//NÃO FUNCIONAR
+
+
+
+
+	//INICIO PARAMETROS GLOBAIS
+
+	//PIXEL FACEBOOK
+	//Preencha com o id do PIXEL, copiado da sua plataforma, 
+	//NÃO APAGUE AS ASPAS DUPLAS DO INÍCIO E DO FIM DO PARÂMETRO;
+	
+	$pixelFacebook = "ID_DO_PIXEL_DO_FACEBOOK";
+	
+
+	//LINK DO WHATSAPP
+	//Preencha com o número do seu whatsapp com o DDD
+	//"38999887766".
+	//NÃO APAGUE AS ASPAS DUPLAS DO INÍCIO E DO FIM DO PARÂMETRO;
+
+	$linkWhats = "38999887766";
+	
+
+	//PARAMETROS EXTRAS
+	$head = "";
+
+	$body = "";
+
+	//FIM PARAMETROS GLOBAIS
+	
+
+
+
+	//INICIO PARAMETROS ADV
+
+	//LINK DA PÁGINA DE VENDA
+	//Preencha com A URL completa
+	//NÃO APAGUE AS ASPAS DUPLAS DO INÍCIO E DO FIM DO PARÂMETRO;
+
+	$linkPV = "https://www.google.com";
+	
+	//FIM PARAMETROS ADV
+	
+
+
+
+	//INICIO PARAMETROS PV
+	
+	//LINK DA PAGINA DE OFERTA
+	//Preencha com A URL completa
+	//NÃO APAGUE AS ASPAS DUPLAS DO INÍCIO E DO FIM DO PARÂMETRO;
+
+	$linkPgOferta = "";
+	
+	
+	//LINK DAS OFERTAS DA PV
+	//Preencha com o LINK completo, copiado da sua plataforma, 
+	//"https://ev.braip.com/checkout...".
+	//NÃO APAGUE AS ASPAS DUPLAS DO INÍCIO E DO FIM DO PARÂMETRO;
+	//ATENÇÃO para os parâmetros abaixo, tem um campo para inserir o link de cada oferta.
+
+	$oferta1 = "LINK_DA_SUA_CAMPANHA_DE_1_FRASCO?split=12";
+
+	
+	//FIM PARAMETROS PV
+	
+	
+	//INICIO PARAMETROS OFERTA
+	
+	//LINK DAS OFERTAS
+	//Preencha com o LINK completo, copiado da sua plataforma, 
+	//"https://ev.braip.com/checkout...".
+	//NÃO APAGUE AS ASPAS DUPLAS DO INÍCIO E DO FIM DO PARÂMETRO;
+	//ATENÇÃO para os parâmetros abaixo, tem um campo para inserir o link de cada oferta.
+
+	$oferta1_2 = "LINK_DA_SUA_OFERTA_COMPRE_1_LEVE_2_FRASCO?split=12";
+
+	//FIM PARAMETROS OFERTA
+
+?>
